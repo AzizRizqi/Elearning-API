666ff29608e9f6cb11a3c15221b81fc0fc7e8169
AzizRizqi <azizrizqi2005@gmail.com>
2025-06-26 19:46:35 +0700

    add GET kursus

--- src/pages/api/kursus/index.ts
+++ src/pages/api/kursus/index.ts
@@ -9,8 +9,17 @@ const client = await clientPromise;
   const db = client.db("elearning");
   switch (req.method) {
     case "GET":
-      const kursus = await db.collection("kursus").find({}).toArray();
-      res.json({ status: 200, data: kursus });
+      const filter = req.query.filter || '';
+      const options = {
+        projection:{
+          content: 0
+        }
+      }
+      const kursus = await db.collection("kursus").find({
+        title:{$regex: filter, $options: 'i'},
+      },options).toArray();
+
+      res.json({status: 200, data:kursus});
       break;
   }
 }
